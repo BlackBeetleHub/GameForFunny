6c4e6b87b458ec9677956ab8561ded44fbb48949
Black Beetle <black.beetle@mail.ru>
2017-03-04 23:21:23 +0300

    vertexData for Sprite box

--- Sprite.cpp
+++ Sprite.cpp
@@ -12,7 +12,26 @@ void Sprite::init(int x, int y, int width, int height)
 		glGenBuffers(1, &_vboID);
 
 	}
-	
+
+	float vertexData[12];
+
+	vertexData[0] = x + width;
+	vertexData[1] = y + height;
+
+	vertexData[2] = x;
+	vertexData[3] = y + height;
+
+	vertexData[4] = x;
+	vertexData[5] = y;
+
+	vertexData[6] = x;
+	vertexData[7] = y;
+
+	vertexData[8] = x + width;
+	vertexData[9] = y;
+
+	vertexData[10] = x + width;
+	vertexData[11] = y + height;
 }
 
 void Sprite::draw()
